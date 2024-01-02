3c74fcaa61410e596a4145b4f8e3b61e78a50f07
apathare9 <ajinkyapathare31@gmail.com>
2024-01-02 22:18:00 +0530

    --> Middleware's

---
+++ Week 2/2.2 - Continuation of Express.js/index.js
@@ -0,0 +1,46 @@
+const express = require('express'); 
+const app = express();
+const port = 3000;
+
+function middleware1(req, res, next){
+    console.log("from inside middleware" + req.headers.counter);
+    // res.send("Error from Middleware 1.")   --> Used to send back error message.
+    next();
+}
+
+app.use(middleware1);
+
+
+function calculateSum(counter) {
+    var sum = 0;
+    for(var i=0; i<=counter; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+
+function handleFirstRequest(req,res) {
+    // var counter = req.query.counter;            // get data(counter) from queries.
+    console.log(req.headers);                       
+    var counter = req.headers.counter;             // get data(counter) in the form of headers. 
+    var calculatedSum = calculateSum(counter);
+    // console.log(calculatedSum);
+    var answer = "The Sum is " + calculatedSum;
+    res.send(answer);
+}
+
+function createuser(req,res){
+    res.send("Hello World.");
+}
+
+// app.get('/handlesum', handleFirstRequest);
+app.post('/handlesum', handleFirstRequest);
+
+function started() {
+    console.log(`Example app listening on port ${port}`);    
+}
+
+app.listen(port, started);
+
