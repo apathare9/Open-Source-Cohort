5a7a6a8c9b543f06434f61d91d73c0570af80ffb
apathare9 <ajinkyapathare31@gmail.com>
2024-01-02 23:54:41 +0530

    Week 2.2 --> Done

--- Week 2/2.2 - Continuation of Express.js/index.js
+++ Week 2/2.2 - Continuation of Express.js/index.js
@@ -15,31 +15,56 @@ app.use(bodyParser.json());
 
 function calculateSum(counter) {
     var sum = 0;
-    for(var i=0; i<=counter; i++){
+    for(var i=1; i<=counter; i++){
         sum = sum + i;
     }
     return sum;
 }
 
+// a => 1 * 2 * 3 * 4 = 24
+// function calculateMul(counter) {
+//     var answer = 1;
+//     for(var i=1; i<=counter; i++){
+//         answer = answer * i;
+//     }
+//     return answer;
+// }
+
 
 
 function handleFirstRequest(req,res) {
-    console.log(req.body);
+
+    var counter = req.query.counter;
+
+    var calculatedSum = calculateSum(counter);
+    // var calculatedMul = calculateMul(counter);
+
+    var answerObject = {
+        sum: calculatedSum,
+        // mul: calculatedMul,
+    };
+
+    res.status(200).send(answerObject);
+
+   
+    // res.status(411).send("You have sent very big number");
+    // var answer = "The Sum is " + calculatedSum;
+
     // var counter = req.query.counter;            // get data(counter) from queries.
     //console.log(req.headers);                      // get data(counter) in the form of headers.  
-    var counter = req.body.counter;             
-    var calculatedSum = calculateSum(counter);
+    // var counter = req.body.counter;             
+    
     // console.log(calculatedSum);
-    var answer = "The Sum is " + calculatedSum;
-    res.send(answer);
+    
 }
 
-function createuser(req,res){
-    res.send("Hello World.");
+function givePage(req, res){
+    res.sendFile( "D:\\Open Source Cohort\\Open-Source-Cohort\\Week 2\\2.2 - Continuation of Express.js\\index.html");
 }
 
 // app.get('/handlesum', handleFirstRequest);
-app.post('/handlesum', handleFirstRequest);
+app.get('/handlesum', handleFirstRequest);
+app.get('/',givePage);   
 
 function started() {
     console.log(`Example app listening on port ${port}`);    
